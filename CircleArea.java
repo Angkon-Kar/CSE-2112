752d7c3362eb2d1c6afc4ed8c062c2cee939c3a8
Angkon Kar <angkon310@gmail.com>
2026-05-04 22:28:44 +0600

    Circle of Area in java.

--- CircleArea.java
+++ CircleArea.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
 public class CircleArea {
    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       System.out.print("Enter the radius of the circle: ");
-       double radius = sc.nextDouble();
-       double area = Math.PI * radius * radius;
-       System.out.println("The area of the circle with radius " + radius + " is: " + area);
+        Scanner sc = new Scanner(System.in);
+        double PI = 3.14159;
+        System.out.print("Enter the radius of the circle: ");
+        double radius = sc.nextDouble();
+        double area = PI * radius * radius;
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+        sc.close();
    }
 }
-
-/*
-Enter the radius of the circle: 5
-The area of the circle with radius 5.0 is: 78.53981633974483
-*/
